63b4e9b82c0203b63d5b06206e593b1c2be5e4ed
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-01-28 17:51:48 +0200

    basic routes added

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,1 +1,12 @@
-export const routes = (app) => {};
+export const routes = (app) => {
+  app
+    .route("/tasks")
+    .get((_req, res) => res.send("GET request works"))
+    .post((_req, res) => res.send("POST request works"));
+
+  app
+    .route("/task/:id")
+    .get((_req, res) => res.send("GET task by id works"))
+    .put((_req, res) => res.send("PUT request works"))
+    .delete((_req, res) => res.send("DELETE request works"));
+};
