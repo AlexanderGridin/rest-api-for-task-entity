2d9fa879bbe09d9ca32df0256f699479e44defa9
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-01-28 18:20:55 +0200

    setup connection to MongoDB

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
 import { routes } from "./src/routes/crmRoutes";
 
 const app = express();
 const PORT = 4004;
+
+// mongoose connection
+mongoose.set("strictQuery", false);
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/TasksDB", { useNewUrlParser: true });
+
+// bodyparser setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 routes(app);
 
